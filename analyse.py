754c875bfe4fb3af5ee7455a3d2ac95a72d93faf
Anna Anioł <15068386+annaaniol@users.noreply.github.com>
2019-04-18 22:38:58 +0200

    Add multiple models

--- analyse.py
+++ analyse.py
@@ -7,12 +7,18 @@ import re
 import argparse
 import sys
 from SQuAD_metrics import SQuAD_metrics
+from plot import Plotter
 import numpy as np
+from collections import OrderedDict
+from operator import itemgetter
 
 bidaf_prediction_file = 'BiDAF/prediction0-epoch7.out'
-bidaf_dev_file = '../BiDAF/BiDAF-pytorch/.data/squad/dev-v1.1.json'
+mnemonic_prediction_file = 'MnemonicReader/SQuAD-dev-v1.1-m_reader.preds'
+rnet_prediction_file = 'R-net/SQuAD-dev-v1.1-r_net.preds'
+dev_pattern_file = '../BiDAF/BiDAF-pytorch/.data/squad/dev-v1.1.json'
 
 metrics = SQuAD_metrics()
+plotter = Plotter()
 
 def determine_type(question):
     question = question.lower()
@@ -46,74 +52,103 @@ def determine_type(question):
         type = 'unknown'
     return type
 
-predictions = {}
-with open(bidaf_prediction_file, 'r', encoding='utf-8') as f:
-    predictions = json.load(f)
+def analyze_model(model_name, model_prediction_file):
+    predictions = {}
+    with open(model_prediction_file, 'r', encoding='utf-8') as f:
+        predictions = json.load(f)
+
+    ground_truths = {}
+    questions = {}
+    questions_ids = {}
+    with open(dev_pattern_file, 'r', encoding='utf-8') as f:
+        data = json.load(f)['data']
+        for article in data:
+            for paragraph in article['paragraphs']:
+                # context = paragraph['context']
+                # tokens = word_tokenize(context)
+                for qa in paragraph['qas']:
+                    id = qa['id']
+                    words = nltk.word_tokenize(qa['question'])
+                    # print(words)
+                    questions_ids[qa['question']] = id
+                    type = determine_type(qa['question'])
+                    questions[id] = (type,qa['question'])
+                    # question = qa['question']
+                    for ans in qa['answers']:
+                        answer = ans['text']
+                        # s_idx = ans['answer_start']
+                        # e_idx = s_idx + len(answer)
+
+                        if id not in ground_truths:
+                            ground_truths[id] = []
+
+                        ground_truths[id].append(answer)
+
+    stats_f1 = {}
+    stats_em = {}
+    f1 = exact_match = total = 0
+    eval_dict = {}
+    for id, prediction in predictions.items():
+        ground_truths_per_question = ground_truths[id]
+        type = questions[id][0]
+        # print(questions[id][1])
+
+        exact_match_here = metrics.metric_max_over_ground_truths(
+            metrics.exact_match_score, prediction, ground_truths_per_question)
+        f1_here = metrics.metric_max_over_ground_truths(
+            metrics.f1_score, prediction, ground_truths_per_question)
+        exact_match += exact_match_here
+        f1 += f1_here
+        eval_dict[qa['id']] = {'em': exact_match_here, 'f1': f1_here}
+        total += 1
+        # print('F1:' + str(f1_here))
+        # print('EM: ' + str(exact_match_here))
+
+        if type in stats_f1:
+            stats_f1[type].append(f1_here)
+            stats_em[type].append(exact_match_here)
+        else:
+            stats_f1[type] = [f1_here]
+            stats_em[type] = [exact_match_here]
+
+    exact_match = 100.0 * exact_match / total
+    f1 = 100.0 * f1 / total
+
+    stats_f1 = OrderedDict(sorted(stats_f1.items(), key = itemgetter(0), reverse = False))
+    stats_em = OrderedDict(sorted(stats_em.items(), key = itemgetter(0), reverse = False))
+
+    print('\ntotal F1 ' + str(f1))
+    for type, f1_list in stats_f1.items():
+        mean_f1 = np.mean(f1_list)
+        print(type + ' ' + str(mean_f1))
+
+    print('\ntotal EM ' + str(exact_match))
+    for type, em_list in stats_em.items():
+        mean_em = np.mean(em_list)
+        print(type + ' ' + str(mean_em))
+
+    result_dict = {}
+    result_dict['f1'] = [stats_f1, f1]
+    result_dict['em'] = [stats_em, exact_match]
+    return result_dict
 
 
-ground_truths = {}
-questions = {}
-questions_ids = {}
-with open(bidaf_dev_file, 'r', encoding='utf-8') as f:
-    data = json.load(f)['data']
-    for article in data:
-        for paragraph in article['paragraphs']:
-            # context = paragraph['context']
-            # tokens = word_tokenize(context)
-            for qa in paragraph['qas']:
-                id = qa['id']
-                words = nltk.word_tokenize(qa['question'])
-                # print(words)
-                questions_ids[qa['question']] = id
-                type = determine_type(qa['question'])
-                questions[id] = (type,qa['question'])
-                # question = qa['question']
-                for ans in qa['answers']:
-                    answer = ans['text']
-                    # s_idx = ans['answer_start']
-                    # e_idx = s_idx + len(answer)
+stats_f1_list = []
+f1_list = []
+stats_em_list = []
+em_list = []
+names_list = []
 
-                    if id not in ground_truths:
-                        ground_truths[id] = []
+models_to_process = [('R-net', rnet_prediction_file), ('Mnemonic Reader', mnemonic_prediction_file), ('BiDAF', bidaf_prediction_file)]
+for model in models_to_process:
+    name = model[0]
+    file = model[1]
+    results = analyze_model(name, file)
+    stats_f1_list.append(results['f1'][0])
+    f1_list.append(results['f1'][1])
+    stats_em_list.append(results['em'][0])
+    em_list.append(results['em'][1])
+    names_list.append(name)
 
-                    ground_truths[id].append(answer)
-
-stats_f1 = {}
-stats_em = {}
-f1 = exact_match = total = 0
-eval_dict = {}
-for id, prediction in predictions.items():
-    ground_truths_per_question = ground_truths[id]
-    type = questions[id][0]
-    # print(questions[id][1])
-
-    exact_match_here = metrics.metric_max_over_ground_truths(
-        metrics.exact_match_score, prediction, ground_truths_per_question)
-    f1_here = metrics.metric_max_over_ground_truths(
-        metrics.f1_score, prediction, ground_truths_per_question)
-    exact_match += exact_match_here
-    f1 += f1_here
-    eval_dict[qa['id']] = {'em': exact_match_here, 'f1': f1_here}
-    total += 1
-    # print('F1:' + str(f1_here))
-    # print('EM: ' + str(exact_match_here))
-
-    if type in stats_f1:
-        stats_f1[type].append(f1_here)
-        stats_em[type].append(exact_match_here)
-    else:
-        stats_f1[type] = [f1_here]
-        stats_em[type] = [exact_match_here]
-
-exact_match = 100.0 * exact_match / total
-f1 = 100.0 * f1 / total
-
-print('\ntotal F1 ' + str(f1))
-for type, f1_list in stats_f1.items():
-    mean_f1 = np.mean(f1_list)
-    print(type + ' ' + str(mean_f1))
-
-print('\ntotal EM ' + str(exact_match))
-for type, em_list in stats_em.items():
-    mean_em = np.mean(em_list)
-    print(type + ' ' + str(mean_em))
+plotter.plot_bar(stats_f1_list, f1_list, names_list, 'F1')
+plotter.plot_bar(stats_em_list, em_list, names_list, 'EM')
---
+++ plot.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Plotter():
+    def __init__(self):
+        pass
+
+    def plot_bar(self, stats_list, total_mean_list, model_name_list, metric_name):
+        plt.rcParams["figure.figsize"] = (16,10)
+        colors = ['r','g', 'grey']
+        line_styles = ['--', '--', '--']
+        distances = [-0.3, 0, 0.3]
+
+        ax = plt.subplot(111)
+
+        for counter, model in enumerate(zip(stats_list, total_mean_list, model_name_list),0):
+            stats = model[0]
+            total_mean = model[1]
+            model_name = model[2]
+            print('plotting ' + str(model_name))
+
+            objects = list(stats.keys())
+            print(objects)
+            y_pos = np.arange(len(objects))
+            mean = []
+            for list_of_values in stats.values():
+                mean.append(np.mean(list_of_values))
+
+
+            ax.bar(y_pos+distances[counter], mean, width=0.3, color=colors[counter], align='center', label='{}'.format(model_name))
+            plt.axhline(y=float(total_mean/100), color=colors[counter], linestyle=line_styles[counter])
+
+        plt.xticks(y_pos, objects, rotation=45)
+        plt.ylabel(metric_name)
+        plt.xlabel('Question type')
+        plt.title(metric_name)
+        plt.legend()
+
+        plt.savefig('{}.jpg'.format(metric_name))
+
+        plt.clf()
